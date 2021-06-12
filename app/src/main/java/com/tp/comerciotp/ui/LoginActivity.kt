596a2e4e1a0a213a74cb98bdd9d69b07a0bc4944
luis gerardo rangel garcia <lrangelg@totalplay.com.mx>
2021-06-11 19:15:32 -0500

    commit with all files

---
+++ app/src/main/java/com/tp/comerciotp/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tp.comerciotp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tp.comerciotp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/tp/comerciotp/ui/LoginFragment.kt
@@ -0,0 +1,58 @@
+package com.tp.comerciotp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.tp.comerciotp.R
+import com.tp.comerciotp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private lateinit var binding: FragmentLoginBinding
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+        isUserLoggedIn()
+        doLogin()
+    }
+
+    private fun isUserLoggedIn() {
+        firebaseAuth.currentUser?.let {
+            findNavController().navigate(R.id.homeScreenFragment)
+        }
+    }
+
+    private fun doLogin(){
+        binding.btnLoginStartSession.setOnClickListener{
+            val email = binding.tietLoginUser.text.toString().trim()
+            val pass = binding.tietLoginPass.text.toString().trim()
+
+            validateCredentials(email, pass)
+            singIn(email, pass)
+
+        }
+    }
+
+    private fun validateCredentials(email : String , pass: String){
+        if(email.isEmpty()){
+            binding.tietLoginUser.error = "E-mail está vacío"
+            return
+        }
+
+        if(pass.isEmpty()){
+            binding.tietLoginPass.error = "Password está vacío"
+            return
+        }
+
+    }
+
+    private fun singIn(email : String , pass : String){
+
+    }
+
+
+}
